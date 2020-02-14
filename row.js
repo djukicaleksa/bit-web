6c6b00a41d94d781059fc45e981cba267ed3201c
BIT Student <classroom@bgit.rs>
2020-02-14 16:52:22 +0100

    web page,layout,etc

---
+++ row.js
@@ -0,0 +1,28 @@
+// var a = [5, 3, false, 8, NaN, undefined];
+
+// for (var i = 0; i < a.length; i = i + 2) {
+//     if (typeof a[i] === "number") {
+//         console.log(a[i]);
+//     }
+// }
+
+
+// for (var i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log("FizzBuzz")
+//     } else if (i % 3 === 0) {
+//         console.log('Fizz');
+//     } else if (i % 5 === 0) {
+
+
+//         console.log("Buzz");
+//     }
+//     else {
+//         console.log(i);
+//     }
+// } 
+var res = "";
+for (var d = 0; d < 3; d++) {
+    res += "* \t"
+    console.log(res);
+}
